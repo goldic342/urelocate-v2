8306c7fd312f85e668aa2e360fd49bc9f07d58f1
goldic <me@goldic.xyz>
2024-12-11 17:30:18 +0400

    Move hero to components/landing, add placeholder img

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,19 @@
-import { Box, Container } from '@chakra-ui/react'
-import Hero from '../components/ui/Hero'
+import { Box, Container, Image } from '@chakra-ui/react'
+import Hero from '../components/landing/Hero'
 
 const HomePage = () => {
   return (
-    <Container maxW={'3xl'}>
-      <Box pt={64}>
+    <Container
+      maxW={'6xl'}
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      flexDir={'column'}
+    >
+      <Box pt={64} maxW={'3xl'}>
         <Hero />
       </Box>
+      <Image src="https://placehold.co/1920x1280" mt={16} />
     </Container>
   )
 }
